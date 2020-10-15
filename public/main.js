144ed0b433a338ddddd0bee42abbd42ffa4503bd
zelusp <agostiniworks@gmail.com>
2020-10-15 13:19:42 -0500

    Remove jquery references to deleted elements

--- public/main.js
+++ public/main.js
@@ -1,12 +1,4 @@
 $(function() {
-  var FADE_TIME = 150; // ms
-  var TYPING_TIMER_LENGTH = 400; // ms
-  var COLORS = [
-    '#e21400', '#91580f', '#f8a700', '#f78b00',
-    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-  ];
-
   // Prompt for setting a username
   var username;
   var connected = false;
@@ -36,11 +28,6 @@ $(function() {
 
   // If the username is valid
   if (username) {
-    $loginPage.fadeOut();
-    $chatPage.show();
-    $loginPage.off('click');
-    $currentInput = $inputMessage.focus();
-
     // Tell the server your username
     socket.emit('add user', username);
   }
